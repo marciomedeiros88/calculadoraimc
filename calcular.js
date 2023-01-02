01cbcfbb2b8e427cbdc0c8516540af5d088f30b3
Márcio Medeiros <118381472+marciomedeiros88@users.noreply.github.com>
2023-01-02 17:56:21 -0300

    ajustando js

--- calcular.js
+++ calcular.js
@@ -16,13 +16,13 @@ function calcularIMC(){
         resultadofinal.innerText += `Seu IMC é ${resultadoIMC2}. Abaixo do peso.`
     }else if(resultadoIMC >=19 && resultadoIMC < 25){
         resultadofinal.innerText += `Seu IMC é ${resultadoIMC2}. Peso normal.`
-    }else if(resultadoIMC >=26 && resultadoIMC < 30){
+    }else if(resultadoIMC >=25 && resultadoIMC < 30){
         resultadofinal.innerText += `Seu IMC é ${resultadoIMC2}. Excesso de peso.`
-    }else if(resultadoIMC >= 31 && resultadoIMC < 35){
+    }else if(resultadoIMC >= 30 && resultadoIMC < 35){
         resultadofinal.innerText += `Seu IMC é ${resultadoIMC2}. Obesidade tipo 1.`
-    }else if(resultadoIMC >= 36 && resultadoIMC < 40){
+    }else if(resultadoIMC >= 35 && resultadoIMC < 40){
         resultadofinal.innerText += `Seu IMC é ${resultadoIMC2}. Obesidade tipo 2.`
-    }else if(resultadoIMC >= 41){
+    }else if(resultadoIMC >= 40){
         resultadofinal.innerText += `Seu IMC é ${resultadoIMC2}. Obesidade tipo 3.`
     }
     limparDados();
